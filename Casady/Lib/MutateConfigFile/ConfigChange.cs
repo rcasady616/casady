676802775de63c6eed2de38af2d2147c74273320
Rick Casady <richard.casady@gmail.com>
2014-06-29 20:29:57 -0700

    Adding ConfigDriver
    Part of the dropbox move to github

---
+++ Casady/Lib/MutateConfigFile/ConfigChange.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace Casady.Lib.MutateConfigFile
+{
+    public class ConfigChange
+    {
+        /// <summary>
+        /// Changes an xml node value in a given file
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="xPathQuery"></param>
+        /// <param name="nodeValue"></param>
+        public static void UpdateConfigFile(string fileName, string xPathQuery, string nodeValue)
+        {
+            if (File.Exists(fileName))
+            {
+                var xmlDoc = new XmlDocument();
+                xmlDoc.Load(fileName);
+                var nodeList = xmlDoc.SelectNodes(xPathQuery);
+                if (nodeList == null)
+                    throw new XmlNodeNotFoundException("", fileName, xPathQuery, nodeValue);
+                if (nodeList.Count == 0)
+                    Console.Write(new XmlNodeNotFoundException("", fileName, xPathQuery, nodeValue));
+                if (nodeList.Count > 1)
+                    Console.Write(new MultipleNodesFoundException("", fileName, xPathQuery, nodeValue));
+
+                nodeList[0].InnerText = nodeValue;
+                xmlDoc.Save(fileName);
+            }
+        }
+
+        /// <summary>
+        /// Changes an xml attribute value in a given file
+        /// </summary>
+        /// <param name="fileName">The xml config to be changed</param>
+        /// <param name="xPathQuery">the xPath query to the node</param>
+        /// <param name="attributeKey">The name of the attribute to be changed</param>
+        /// <param name="attributeValue">The new attribute value</param>
+        public static void UpdateConfigFile(string fileName, string xPathQuery, string attributeKey, string attributeValue)
+        {
+            if (!File.Exists(fileName))
+                throw new FileNotFoundException("", fileName);
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(fileName);
+            var nodeList = xmlDoc.SelectNodes(xPathQuery);
+            if (nodeList == null)
+                throw new XmlNodeNotFoundException("", fileName, xPathQuery, attributeKey, attributeValue);
+            if (nodeList.Count == 0)
+                Console.Write(new XmlNodeNotFoundException("", fileName, xPathQuery, attributeKey, attributeValue));
+            if (nodeList.Count > 1)
+                Console.Write(new MultipleNodesFoundException("", fileName, xPathQuery, attributeKey, attributeValue));
+
+            foreach (XmlNode node in nodeList)
+            {
+                if (node.Attributes != null && node.Attributes[attributeKey] != null)
+                    node.Attributes[attributeKey].Value = attributeValue;
+                else
+                    Console.Write(new XmlAttributesNotFoundException("", fileName, xPathQuery, attributeKey, attributeValue));
+            }
+            xmlDoc.Save(fileName);
+        }
+
+    }
+
+    public class XmlNodeNotFoundException : XmlException
+    {
+        public XmlNodeNotFoundException()
+        {
+
+        }
+        public XmlNodeNotFoundException(string exceptionText)
+            : base(string.Format("XmlNodeNotFoundException: {0}", exceptionText))
+        {
+        }
+        public XmlNodeNotFoundException(string exceptionText, string fileName, string xPathQuery, string nodeValue)
+            : base(string.Format("XmlNodeNotFoundException: {0}\nFile name: {1}\nXPath query: {2}\nNode value: {3}", exceptionText, fileName, xPathQuery, nodeValue))
+        {
+        }
+        public XmlNodeNotFoundException(string exceptionText, string fileName, string xPathQuery, string attributekey, string attributeValue)
+            : base(string.Format("XmlNodeNotFoundException: {0}\nFile name: {1}\nXPath Query: {2}\nAttribute key: {3}\nAttribute Value:{4}", exceptionText, fileName, xPathQuery, attributekey, attributeValue))
+        {
+        }
+    }
+
+    public class MultipleNodesFoundException : XmlException
+    {
+        public MultipleNodesFoundException()
+        {
+
+        }
+        public MultipleNodesFoundException(string exceptionText)
+            : base(string.Format("MultipleNodesFoundException: {0}", exceptionText))
+        {
+        }
+        public MultipleNodesFoundException(string exceptionText, string fileName, string xPathQuery, string nodeValue)
+            : base(string.Format("MultipleNodesFoundException: {0}\nFile name: {1}\nXPath query: {2}\nNode value: {3}", exceptionText, fileName, xPathQuery, nodeValue))
+        {
+        }
+        public MultipleNodesFoundException(string exceptionText, string fileName, string xPathQuery, string attributekey, string attributeValue)
+            : base(string.Format("MultipleNodesFoundException: {0}\nFile name: {1}\nXPath Query: {2}\nAttribute key: {3}\nAttribute Value:{4}", exceptionText, fileName, xPathQuery, attributekey, attributeValue))
+        {
+        }
+    }
+
+    public class XmlAttributesNotFoundException : XmlException
+    {
+        public XmlAttributesNotFoundException()
+        {
+
+        }
+        public XmlAttributesNotFoundException(string exceptionText)
+            : base(string.Format("XmlAttributesNotFoundException: {0}", exceptionText))
+        {
+        }
+        public XmlAttributesNotFoundException(string exceptionText, string fileName, string xPathQuery, string nodeValue)
+            : base(string.Format("XmlAttributesNotFoundException: {0}\nFile name: {1}\nXPath query: {2}\nNode value: {3}", exceptionText, fileName, xPathQuery, nodeValue))
+        {
+        }
+        public XmlAttributesNotFoundException(string exceptionText, string fileName, string xPathQuery, string attributekey, string attributeValue)
+            : base(string.Format("XmlAttributesNotFoundException: {0}\nFile name: {1}\nXPath Query: {2}\nAttribute key: {3}\nAttribute Value:{4}", exceptionText, fileName, xPathQuery, attributekey, attributeValue))
+        {
+        }
+    }
+}
---
+++ Casady/Tools/ConfigDriver/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Casady.Lib.MutateConfigFile;
+
+namespace Casady.Tools.ConfigDriver
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintTitleMessage();
+            if (args.Length == 1) // by file
+                foreach (var val in GetConfigValues(args[0]))
+                    ChangeConfigOption(val);
+            else if (args.Length == 3)
+                ConfigChange.UpdateConfigFile(args[0], args[1], args[2]);
+            else if (args.Length == 4)
+                ConfigChange.UpdateConfigFile(args[0], args[1], args[2], args[3]);
+            else
+                PrintUsage();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        private static void ChangeConfigOption(List<string> val)
+        {
+            if (val.Count == 4)
+                ConfigChange.UpdateConfigFile(val[0], val[1], val[2], val[3]);
+            else if (val.Count == 3)
+                ConfigChange.UpdateConfigFile(val[0], val[1], val[2]);
+            else
+                Console.Write(new ArgumentException("Incorrect number of arguments passed"));
+        }
+
+        /// <summary>
+        /// Gets the change list from the change file
+        /// </summary>
+        /// <param name="changeFile"></param>
+        /// <returns></returns>
+        public static List<List<string>> GetConfigValues(string changeFile)
+        {
+            if (!File.Exists(changeFile))
+                Console.WriteLine(new FileNotFoundException("", changeFile));
+
+            var ret = new List<List<string>>();
+            var r = new StreamReader(changeFile);
+            while (!r.EndOfStream)
+            {
+                var val = new List<string>();
+                val.AddRange(r.ReadLine().Split('|'));
+                ret.Add(val);
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// Prints title befor changeing the xml config
+        /// </summary>
+        public static void PrintTitleMessage()
+        {
+            Console.WriteLine(@"ConfigDriver.exe is a tool for automating application configuration");
+            Console.WriteLine(@"This program was written by Richard Casady for personal use only");
+        }
+
+        /// <summary>
+        /// Print ConfigDriver.exe usage
+        /// </summary>
+        public static void PrintUsage()
+        {
+            const string sampleXml = "<ConfigDriver>\n  <SampleOne>Change one node value</SampleOne>\n  <SampleTwo>\n    <Attrib name=\"item1\" description=\"Change one specific attribute value\" />\n    <Attrib name=\"item2\" description=\"\" />\n  </SampleTwo>\n  <SampleThree>\n  </SampleThree>\n  <SampleThree name=\"SpecificNone\">Change one specific node value</SampleThree>\n</ConfigDriver>\n";
+            const string changeFile = "..\\..\\sampleFile.xml|/ConfigDriver/SampleOne|Change one node value\n..\\..\\sampleFile.xml|/ConfigDriver/SampleTwo/Attrib[@name='item1']|description|Change one specific attribute value\n..\\..\\sampleFile.xml|/ConfigDriver/SampleThree[@name='SpecificNone']|Change one node value by name\n";
+            Console.WriteLine("------------------------------------------------\n");
+            Console.WriteLine("There are two mode of use: One is to pass the change vales at command line");
+            Console.WriteLine("The second is to put a group of chnage values in a change file.");
+            Console.WriteLine("------------------------------------------------\n");
+            Console.WriteLine(@"Sample usage mode one:");
+            Console.WriteLine("ConfigDriver.exe \"..\\..\\sampleFile.xml\" \"/ConfigDriver/SampleOne\" \"Change one node value\"");
+            Console.WriteLine("ConfigDriver.exe \"..\\..\\sampleFile.xml\" \"/ConfigDriver/SampleTwo/Attrib[@name='item1']\" \"description\" \"Change one specific attribute value\"");
+            Console.WriteLine("ConfigDriver.exe \"..\\..\\sampleFile.xml\" \"/ConfigDriver/SampleThree[@name='SpecificNone']\" \"Change one node value by name\"");
+            Console.WriteLine("------------------------------------------------\n");
+            Console.WriteLine(@"Sample usage Change File usage:");
+            Console.WriteLine(@"ConfigDriver.exe C:\ChangeFile.txt");
+            Console.WriteLine("------------------------------------------------\n");
+            Console.WriteLine("File layout definition:");
+            Console.WriteLine("To change a Single node value");
+            Console.WriteLine("File Name Path | XPath Query | Node Value");
+            Console.WriteLine("To change an attribute value");
+            Console.WriteLine("File Name Path | XPath Query | Attribute Name | Attribute Value");
+            Console.WriteLine("The parameters are \"|\" delimited");
+            Console.WriteLine("------------------------------------------------\n");
+            Console.WriteLine("Sample change file:");
+            Console.Write(changeFile);
+            Console.WriteLine("------------------------------------------------\n");
+            Console.WriteLine("Sample resulting file:");
+            Console.Write(sampleXml);
+            Console.WriteLine("------------------------------------------------\n");
+        }
+    }
+}
